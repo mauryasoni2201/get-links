9d391f49548ec4e9562d8b9da28677af1681686a
maurya2201 <maurya@krishaweb.com>
2025-03-25 19:08:41 +0530

    add cors.

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors('*'));
 app.use(morgan("dev"));
 app.use(express.json());
 
